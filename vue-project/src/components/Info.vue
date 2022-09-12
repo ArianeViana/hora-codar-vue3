dd853377307d7279501d5b0fdd4a5e704d85a8b2
Ariane Viana da Silva <arianeviana.dsn.pack@alterdata.matriz>
2022-09-12 17:43:58 -0300

    diretivas

--- vue-project/src/components/Info.vue
+++ vue-project/src/components/Info.vue
@@ -1,5 +1,6 @@
 <template>
-    <p>Estou trabalhando no momento</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <p>Utilizo as seguintes tecnologias</p>
     <ul>
         <li>Java</li>
@@ -7,10 +8,18 @@
         <li>C#</li>
         <li>Vue.js</li>
     </ul>
+    <p v-show="mandar_email">Mande uma mensagem para: {{email}} </p>
 </template>
 
 <script>
 export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: false,
+            email: "ariane@email.com"
+        }
+    }
 }
 </script>
