c2783e03ce7a53c6106603b7128cdded620c8bb3
Ariane Viana da Silva <arianeviana.dsn.pack@alterdata.matriz>
2022-09-14 12:25:55 -0300

    primeiros passos

---
+++ projeto-curso/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-container">
+    <Banner />
+    <h1>Monte o seu Burguer:</h1>
+    <Form />
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue';
+import Form from '../components/Form.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+    Form
+  }
+}
+
+</script>
+
